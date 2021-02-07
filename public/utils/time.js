8d4ed2248a36a2f80f2a69ff049078d320b5108b
Alexey Nurgaliev <atnurgaliev@gmail.com>
2021-02-07 22:01:10 +0300

    minutes fix

--- public/utils/time.js
+++ public/utils/time.js
@@ -2,7 +2,7 @@ export default {
 
     formatTime(time) {
         const intSeconds = Math.trunc(time);
-        const minutes = Math.trunc(intSeconds / 60);
+        const minutes = Math.trunc(intSeconds / 60) % 60;
         const hours = Math.trunc(intSeconds / 3600);
         const frac = time - intSeconds;
         const seconds = intSeconds % 60 + Math.round(frac * 1000) / 1000;
