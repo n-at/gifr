9f9f54b073f1040e915cb59e1fbea3feb8d0cd13
Alexey Nurgaliev <atnurgaliev@gmail.com>
2021-05-05 17:36:19 +0300

    ffmpeg and ffprobe binaries search

--- src/main/java/ru/doublebyte/gifr/Application.java
+++ src/main/java/ru/doublebyte/gifr/Application.java
@@ -1,5 +1,8 @@
 package ru.doublebyte.gifr;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +12,19 @@ import ru.doublebyte.gifr.configuration.FFMPEGParams;
 import ru.doublebyte.gifr.struct.CommandlineArguments;
 
 import javax.annotation.PreDestroy;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Value("${server.address}")
+    private String serverHost;
+
+    @Value("${server.port}")
+    private String serverPort;
 
     private final FileManipulation fileManipulation;
     private final CommandlineExecutor commandlineExecutor;
@@ -31,14 +44,61 @@ public class Application implements CommandLineRunner {
     public void run(String... args) {
         fileManipulation.ensureDirectoriesExist();
         cleanupTmpFiles();
-        commandlineExecutor.execute(new CommandlineArguments(ffmpegParams.getFFMPEGBinary()).add("-version"));
-        commandlineExecutor.execute(new CommandlineArguments(ffmpegParams.getFFProbeBinary()).add("-version"));
+
+        if (!check(ffmpegParams.getFFMPEGBinary())) {
+            var ffmpeg = Paths.get("ffmpeg");
+            if (!Files.exists(ffmpeg)) {
+                ffmpeg = Paths.get("ffmpeg.exe");
+                if (!Files.exists(ffmpeg)) {
+                    throw new RuntimeException("ffmpeg binary not found");
+                }
+            }
+
+            var ffmpegAbsolutePath = ffmpeg.toAbsolutePath().toString();
+            if (!check(ffmpegAbsolutePath)) {
+                throw new RuntimeException("ffmpeg check failed");
+            }
+
+            ffmpegParams.setFFMPEGBinary(ffmpegAbsolutePath);
+        }
+
+        if (!check(ffmpegParams.getFFProbeBinary())) {
+            var ffprobe = Paths.get("ffprobe");
+            if (!Files.exists(ffprobe)) {
+                ffprobe = Paths.get("ffprobe.exe");
+                if (!Files.exists(ffprobe)) {
+                    throw new RuntimeException("ffprobe binary not found");
+                }
+            }
+
+            var ffprobeAbsolutePath = ffprobe.toAbsolutePath().toString();
+            if (!check(ffprobeAbsolutePath)) {
+                throw new RuntimeException("ffprobe check failed");
+            }
+
+            ffmpegParams.setFFProbeBinary(ffprobeAbsolutePath);
+        }
+
+        logger.info("");
+        logger.info("");
+        logger.info("http://{}:{}/", serverHost, serverPort);
+        logger.info("");
+        logger.info("");
     }
 
     @PreDestroy
     private void cleanupTmpFiles() {
         fileManipulation.removeEncodedChunks();
         fileManipulation.removeExportFiles();
+    }
+
+    private boolean check(String binary) {
+        try {
+            commandlineExecutor.execute(new CommandlineArguments(binary).add("-version"));
+            return true;
+        } catch (Exception ignored) {
+            return false;
+        }
     }
 
 }
